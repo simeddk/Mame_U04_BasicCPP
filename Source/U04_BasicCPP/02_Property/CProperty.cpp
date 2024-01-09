af68ad317adab5589dc609bb1d9c5a8ae6d9bfe4
KS Kim <65766852+simeddk@users.noreply.github.com>
2024-01-09 17:32:21 +0900

    0108_UPROPERTY

---
+++ Source/U04_BasicCPP/02_Property/CProperty.cpp
@@ -0,0 +1,20 @@
+#include "CProperty.h"
+
+ACProperty::ACProperty()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ACProperty::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ACProperty::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
